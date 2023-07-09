0a1eeab179adc52b8647ce14e9c5167c62ea3845
paulaji <paulajiparayil123@gmail.com>
2023-07-09 12:16:40 +0530

    POST method is enabled

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     return render_template("index.html")
 
-@app.route('/about')
-def about():
-    return render_template('about.html')
+@app.route('/edit', methods = ['GET', 'POST'])
+def edit():
+    if request.method == "POST":
+        return "POST is now enabled, you can post your photos"
+    return render_template('index.html')
 
 app.run(debug=True, port = 5001)
