630cf86327640448e6834d3291d5258ebd25a9d3
paulaji <paulajiparayil123@gmail.com>
2023-07-10 12:05:03 +0530

    RGB to grayscale working

--- main.py
+++ main.py
@@ -1,13 +1,21 @@
 import os
 from flask import Flask, render_template, request, flash
 from werkzeug.utils import secure_filename
-UPLOAD_FOLDER = './static'
+import cv2
+UPLOAD_FOLDER = './uploads'
 ALLOWED_EXTENSIONS = {'png', 'jpeg', 'webp', 'jpg'}
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
-def processImage():
+def processImage(filename, operation):
+    print(f"the filename {filename} was performed an operation {operation}")
+    img = cv2.imread(f"./uploads/{filename}")
+    match operation:
+        case 'cgray': 
+            # using cvtColor (convert color) of cv2 library to do manipulations
+            imgProcessed = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+            cv2.imwrite(f"./static/{filename}", imgProcessed)
     pass
 
 @app.route('/')
@@ -22,6 +30,7 @@ def allowed_file(filename):
 @app.route('/upload', methods = ['GET', 'POST'])
 def upload():
     if request.method == 'POST':
+        operation = request.form.get("operation")
         # check if the post request has the file part
         if 'file' not in request.files:
             flash('No file part')
@@ -35,7 +44,7 @@ def upload():
         if file and allowed_file(file.filename):
             filename = secure_filename(file.filename)
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            # processImage()
+            processImage(filename, operation)
             return render_template("index.html")
     return render_template('index.html')
 
